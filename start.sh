9d7d10ea3576437b91b79ba39fc477d16793e519
Mike Tums <mktums@gmail.com>
2015-10-07 07:01:57 +0300

    added start.sh script

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+########################################
+# Start script for Husky test project. #
+########################################
+
+# Let's start with updating packages list!
+apt-get update
+
+# Python 3 is already installed by default in Ubuntu 14.04 But pip isn't.
+# Let's change it!
+apt-get install -y python3-pip
+
+# Now we need to install required packages. From definition of test project
+# we assume that our project will be used in Docker container, so we actually
+# don't need to isolate our project.
+#
+# Therefore we can actually install Django system-wide
+pip3 install Django==1.8.4
+
+# Applying migrations
+python3 manage.py migrate
+
+# Run server
+#
+# Note: I can't test it by myself, since my router doesn't work well with VMs.
+# Tho tests with port forwarding says that everything is alright.
+python3 manage.py runserver 0.0.0.0:80
